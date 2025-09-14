c4504d2653271a2712b565e3986d73307d96aa6c
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-09-14 20:45:30 +0900

    chore : 기능 주석 작성

--- hw1/memo/notepad9.py
+++ hw1/memo/notepad9.py
@@ -1,9 +1,11 @@
-from importlib.resources import path
+# from importlib.resources import path
 import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QPlainTextEdit, QAction, qApp, QMessageBox, QFileDialog)
 
 def about_dialog():
+    # About 대화상자 표시
+    # QMessageBox.about을 사용해 모달 다이얼로그 생성 부모로 전연 window를 사용(현 인스턴스 참조)
     text = "<center>" \
         "<h1>Simple Notepad</h1>"\
         "&#8291;" \
@@ -15,33 +17,36 @@ def about_dialog():
 
 
 class MainWindow(QMainWindow):
+    # 메인 윈도우 구현
+    # QMainwindow를 상속. 중앙 위젯에 QPlainTextEdit 배치 
     def __init__(self):
         super().__init__()
-
+        # 간단한 텍스트 편집을 위한 위젯
         self.text_widget = QPlainTextEdit()
         self.setCentralWidget(self.text_widget)
-
+        # 타이틀 / 레이아웃
         self.title = 'Simple NotePad'
-        self.window_icon = 'pagichacha3.png'
         self.left = 300
         self.top = 100
         self.width = 700
         self.height = 800
-
+        # 각 메뉴
         self.quit_action = QAction('&Quit', self)
         self.about_action = QAction('&About', self)
         self.open_action = QAction('&Open', self)
         self.save_action = QAction('&Save', self)
         self.save_as_action = QAction('&Save as', self)
-
+        # 디버그 로그(숫자들을 통해 어디에서 오류가 나는지 확인 가능)
         print("5")
-
+        # 현재 열린 파일 경로 저장
         self.file_path = None
-
+        # 단축키 / 시그널 연결
         self.create_actions()
+        #  UI 초기화
         self.init_ui()
 
     def create_menubar(self):
+        # 메뉴바, 메뉴 항목 구성
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
 
@@ -52,6 +57,7 @@ class MainWindow(QMainWindow):
         self.help_menu.addAction(self.about_action)
 
     def create_actions(self):
+        # 단축키와 동작 연결
         pass
         self.quit_action.setShortcut('Ctrl+Q')
         self.quit_action.triggered.connect(qApp.quit)
@@ -68,6 +74,7 @@ class MainWindow(QMainWindow):
         self.save_as_action.triggered.connect(self.save_as_file)
 
     def open_file(self):
+        # 파일 열기
         global path
         path = QFileDialog.getOpenFileName(window, "Open")[0]
         print(path,"++++++++++++++++++++++++++ 9")
@@ -79,6 +86,7 @@ class MainWindow(QMainWindow):
             self.file_path = path
 
     def save_file(self):
+        # 파일 저장
         # print(self.file_path,"+++++++++++++++++ 10")
         if self.file_path is None:
             print("1")
@@ -92,6 +100,7 @@ class MainWindow(QMainWindow):
 
 
     def save_as_file(self):
+        # 현재 편집 내용 파일에 저장
         pass
         path = QFileDialog.getSaveFileName(window, 'Save As')[0]
         print(path)
@@ -105,12 +114,12 @@ class MainWindow(QMainWindow):
 
 
     def init_ui(self):
+        # 메뉴 객체 생성 / 배치
         self.file_menu = self.menuBar().addMenu("&File")
         self.help_menu = self.menuBar().addMenu("&Help")
         self.create_menubar()
 
         self.setWindowTitle(self.title)
-        self.setWindowIcon(QIcon(self.window_icon))
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setFixedSize(self.width, self.height)
 
@@ -119,6 +128,7 @@ class MainWindow(QMainWindow):
         print("4")
 
 if __name__ == "__main__":
+    #  메인 윈도우 인스턴스화
     app = QApplication(sys.argv)
     print("1")
     window = MainWindow()
